f182dadee0c3512871027e06bc38535206605a57
Hrvoje Gazibara <hgazibara@gmail.com>
2016-01-01 23:45:40 +0100

    Makes test for next location of start of loop token pass

--- esoterics/brainfuck/__init__.py
+++ esoterics/brainfuck/__init__.py
@@ -5,7 +5,21 @@
 """
 
 def tokenize(code):
-    return [Token(command, position + 1) for position, command in enumerate(code)]
+    loop_starts = []
+    tokens = []
+
+    for position, command in enumerate(code):
+        if command == '[':
+            loop_starts.append(position)
+
+        if command == ']':
+            loop_start = loop_starts.pop()
+            tokens.append(Token(command, loop_start))
+            tokens[loop_start].next = position
+        else:
+            tokens.append(Token(command, position + 1))
+
+    return tokens
 
 
 class Token(object):
