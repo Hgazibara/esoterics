7e6d0dfa3b83d5b2f9ff2329fd04be2ca6ac3cbd
Hrvoje Gazibara <hgazibara@gmail.com>
2016-01-02 13:05:36 +0100

    Moves tokenizer code into a special module

--- esoterics/brainfuck/__init__.py
+++ esoterics/brainfuck/__init__.py
@@ -1,27 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-def tokenize(code):
-    loop_starts = []
-    tokens = []
-
-    for position, command in enumerate(code):
-        if command == '[':
-            loop_starts.append(position)
-
-        if command == ']':
-            loop_start = loop_starts.pop()
-            tokens.append(Token(command, loop_start))
-            tokens[loop_start].next = position
-        else:
-            tokens.append(Token(command, position + 1))
-
-    return tokens
-
-
-class Token(object):
-
-    def __init__(self, command, next_position):
-        self.command = command
-        self.next = next_position
+from tokenizer import tokenize, Token
