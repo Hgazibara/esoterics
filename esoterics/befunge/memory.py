b0413da904199f6a995242eeb996ad168708ec23
Hrvoje Gazibara <hgazibara@gmail.com>
2016-01-03 19:15:43 +0100

    Initial implementation of befunge-93 memory and pointer classes

--- esoterics/befunge/memory.py
+++ esoterics/befunge/memory.py
@@ -10,9 +10,27 @@ class Memory(object):
     def __init__(self):
         self.cells = [[None] * self.MAX_Y for _ in xrange(self.MAX_X)]
 
+    def fill(self, code):
+        for i, row in enumerate(code):
+            for j, cell in enumerate(row):
+                self.cells[i][j] = cell
+
 
 class Pointer(object):
+
+    directions = {
+        '>': (0, 1),
+        '<': (0, -1),
+        '^': (-1, 0),
+        'v': (1, 0)
+    }
 
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.direction = '>'
+
+    def move(self):
+        x, y = self.directions[self.direction]
+        self.x = (self.x + x) % Memory.MAX_X if (self.x + x) >= 0 else Memory.MAX_X + x
+        self.y = (self.y + y) % Memory.MAX_Y if (self.y + y) >= 0 else Memory.MAX_Y + y
